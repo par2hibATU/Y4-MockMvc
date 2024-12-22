48898c60ac58cc0673f595ad9ec42359479d373f
par2hibATU <parthibbdcox@gmail.com>
2024-12-22 21:53:48 +0000

    Testing

--- src/test/java/ie/atu/week8/projectexercise/ProductControllerTest.java
+++ src/test/java/ie/atu/week8/projectexercise/ProductControllerTest.java
@@ -1,14 +1,33 @@
 package ie.atu.week8.projectexercise;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(ProductController.class)
 class ProductControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private ProductService productService;
+    private ObjectMapper objectMapper;
+
 
     @BeforeEach
     void setUp() {
+        objectMapper = new ObjectMapper();
     }
 
     @Test
@@ -16,7 +35,14 @@ class ProductControllerTest {
     }
 
     @Test
-    void testGetProductById() {
+    void testGetProductById() throws Exception {
+        Product p = new Product(1L, "name", "desc", 100);
+        when(productService.getProductById(1L)).thenReturn(Optional.of(p));
+
+        mockMvc.perform(get("/products/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("name"))
+                .andExpect(jsonPath("$.description").value("desc"));
     }
 
     @Test
